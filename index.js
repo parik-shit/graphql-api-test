c7e37ebb447ddc1ec6f08accae2dba233e769b71
parik-shit <62713994+parik-shit@users.noreply.github.com>
2024-01-16 20:52:38 +0530

    efefe

--- index.js
+++ index.js
@@ -1,63 +1,36 @@
 // server.js
-import express from 'express';
-import { expressMiddleware } from '@apollo/server/express4';
-import mongoose from 'mongoose';
-import {typeDefs} from './schema.js';
-import {resolvers} from './resolvers.js';
 import { ApolloServer } from '@apollo/server';
+import { express as expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import http from 'http';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { typeDefs, resolvers } from './schema';
 
 const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
 
-const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
 
+// MongoDB Atlas connection
+mongoose.connect('mongodb+srv://parikshit152018:vDkH12x5LPPgFdia@cluster0.l1xtqtu.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+app.use(
+  '/graphql',
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
 
-  // server.start().then(() => {
-  //   server.applyMiddleware({ app });
-  
-  //   const PORT = process.env.PORT || 3000;
-  
-  //   mongoose
-  //     .connect('mongodb+srv://parikshit152018:vDkH12x5LPPgFdia@cluster0.l1xtqtu.mongodb.net/?retryWrites=true&w=majority', {
-  //       useNewUrlParser: true,
-  //       useUnifiedTopology: true,
-  //     })
-  //     .then(() => {
-  //       app.listen(PORT, () => {
-  //         console.log(`Server listening on port ${PORT}`);
-  //       });
-  //     })
-  //     .catch((error) => console.error('Error connecting to MongoDB:', error));
-  // });
-
-
-  const PORT = process.env.PORT || 3000;
-  await server.start();
-  app.use(
-    
-    '/graphql',
-    cors(),
-    express.json(),
-    expressMiddleware(server, {
-      context: async ({ req }) => ({ token: req.headers.token }),
-    }),
-    
-    
-  
-    mongoose
-      .connect('mongodb+srv://parikshit152018:vDkH12x5LPPgFdia@cluster0.l1xtqtu.mongodb.net/?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        app.listen(PORT, () => {
-          console.log(`Server listening on port ${PORT}`);
-        });
-      })
-      .catch((error) => console.error('Error connecting to MongoDB:', error))
-  );
-  
-  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
