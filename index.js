6796990221cc02280efccece92d56d75fcfd650b
parik-shit <62713994+parik-shit@users.noreply.github.com>
2024-01-16 19:20:39 +0530

    imports and imports aah

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import mongoose from 'mongoose';
-import typeDefs from './schema.js';
+import {typeDefs} from './schema.js';
 import resolvers from './resolvers.js';
 
 const app = express();
