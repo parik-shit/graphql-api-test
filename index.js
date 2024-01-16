6ce15d6902ba6bf474f5471cdd21eefed0c0d41f
parik-shit <62713994+parik-shit@users.noreply.github.com>
2024-01-16 19:39:00 +0530

    in accordance with the apollo server version 3

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ const app = express();
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.applyMiddleware({ app });
 
-const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect('mongodb+srv://parikshit152018:vDkH12x5LPPgFdia@cluster0.l1xtqtu.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
+  server.start().then(() => {
+    server.applyMiddleware({ app });
+  
+    const PORT = process.env.PORT || 3000;
+  
+    mongoose
+      .connect('mongodb+srv://parikshit152018:vDkH12x5LPPgFdia@cluster0.l1xtqtu.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        app.listen(PORT, () => {
+          console.log(`Server listening on port ${PORT}`);
+        });
+      })
+      .catch((error) => console.error('Error connecting to MongoDB:', error));
+  });
