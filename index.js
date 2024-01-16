f3b23ca49955e87baa0f04008446db8a143db57a
parik-shit <62713994+parik-shit@users.noreply.github.com>
2024-01-16 19:56:10 +0530

    apollo server 4

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // server.js
 import express from 'express';
-import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import mongoose from 'mongoose';
 import {typeDefs} from './schema.js';
 import {resolvers} from './resolvers.js';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
@@ -11,10 +15,37 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 
 
-  server.start().then(() => {
-    server.applyMiddleware({ app });
+  // server.start().then(() => {
+  //   server.applyMiddleware({ app });
   
-    const PORT = process.env.PORT || 3000;
+  //   const PORT = process.env.PORT || 3000;
+  
+  //   mongoose
+  //     .connect('mongodb+srv://parikshit152018:vDkH12x5LPPgFdia@cluster0.l1xtqtu.mongodb.net/?retryWrites=true&w=majority', {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //     })
+  //     .then(() => {
+  //       app.listen(PORT, () => {
+  //         console.log(`Server listening on port ${PORT}`);
+  //       });
+  //     })
+  //     .catch((error) => console.error('Error connecting to MongoDB:', error));
+  // });
+
+
+  const PORT = process.env.PORT || 3000;
+  await server.start();
+  app.use(
+    
+    '/graphql',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+    
+    
   
     mongoose
       .connect('mongodb+srv://parikshit152018:vDkH12x5LPPgFdia@cluster0.l1xtqtu.mongodb.net/?retryWrites=true&w=majority', {
@@ -26,5 +57,8 @@ const server = new ApolloServer({ typeDefs, resolvers });
           console.log(`Server listening on port ${PORT}`);
         });
       })
-      .catch((error) => console.error('Error connecting to MongoDB:', error));
-  });
+      .catch((error) => console.error('Error connecting to MongoDB:', error))
+  );
+  
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
